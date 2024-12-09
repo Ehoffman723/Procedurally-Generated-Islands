71a7fcfd210ed1258f72473f3510b043a211ffac
Ehoffman723 <ethan.hoffman@bsu.edu>
2024-12-09 03:24:28 -0500

    Fix warnings and call_deffered error

--- treasure.gd
+++ treasure.gd
@@ -7,9 +7,12 @@ func _ready() -> void:
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
+func _process(_delta: float) -> void:
 	pass
 
 
 func _on_area_2d_body_entered(body: Node2D) -> void:
+	call_deferred("switch_screens")
+
+func switch_screens():
 	get_tree().change_scene_to_file("res://end_screen.tscn")
