cccab0e710fc5cceb1384af84b8f53bab4c4d102
Ehoffman723 <ethan.hoffman@bsu.edu>
2024-12-09 02:06:10 -0500

    add treasure chest that spawns on island

--- world.gd
+++ world.gd
@@ -1,4 +1,31 @@
 extends Node2D
 
-func _ready() -> void:
-	pass
+@export var treasure : PackedScene
+@export var tile_map : TileMapLayer
+
+func _ready():
+	spawn_treasure()
+
+func spawn_treasure():
+	var map_size = tile_map.get_used_rect()
+	
+	var found_valid_tile = false
+	var random_x : int
+	var random_y : int
+	
+	while found_valid_tile == false:
+		
+		random_x = randi() % map_size.size.x
+		random_y = randi() % map_size.size.y
+		
+		var tile_id = tile_map.get_cell_atlas_coords(Vector2i(random_x, random_y))
+		
+		print("Tile Coordinates: ", tile_map.map_to_local(Vector2(random_x, random_y)))
+		print("Tile Coordinates: ", Vector2(random_x, random_y))
+		print("Tile Atlas Coords: ", tile_id)
+		
+		if tile_id == Vector2i(0,0) or tile_id == Vector2i(1,0) or tile_id == Vector2i(2,0):
+			var treasure_chest = treasure.instantiate()
+			treasure_chest.position = tile_map.map_to_local(Vector2(random_x, random_y))
+			add_child(treasure_chest)
+			found_valid_tile = true
